98b312e2de60e56c6d986d2518eb4b9414ff1e06
Mehmed Kadric <kadricze@gmail.com>
2024-04-08 10:48:21 +0200

    New form

--- main/forms.py
+++ main/forms.py
@@ -3,10 +3,11 @@
 from django import forms
 from .models import Search
 
+
 class SearchForm(forms.ModelForm):
     class Meta:
         model = Search
         fields = ['query']
         widgets = {
-            'query': forms.TextInput(attrs={'placeholder': 'Enter text here'})
+            'query': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter text here'})
         }
--- main/views.py
+++ main/views.py
@@ -1,18 +1,37 @@
+from time import sleep
+import threading
 from django.shortcuts import render
 from .forms import SearchForm
 from faker import Faker
+from .models import Search
+
+
 fake = Faker()
 
 
-def search(request):
-    if request.method == 'POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            form.save()
-            dummy_data = fake.text()
-            query = request.POST['query']
-            return render(request, 'search.html', {'form': form, 'dummy_data': dummy_data, 'query': query})
-    else:
-        form = SearchForm()
+def save_search(form):
+    sleep(3) # This is just to make this task long-running
+    form.save()
 
-    return render(request, 'search.html', {'form': form})
+
+def search(request):
+    context = {}
+    form = SearchForm(request.GET or None)
+
+    if form.is_valid():
+
+        # Option 1 - Create a thread to save the query in the background
+        background_thread = threading.Thread(target=save_search, args=(form,))
+        background_thread.start()
+
+        # Option 2 - Save the query synchronously
+        # save_search(form)
+
+        dummy_data = fake.text()
+        query = form.cleaned_data['query']
+        context['dummy_data'] = f"{query} {dummy_data}"
+
+    context['form'] = form
+
+    context['forms_no'] = Search.objects.all()
+    return render(request, 'search.html', context=context)
